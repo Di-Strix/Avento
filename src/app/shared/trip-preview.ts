26cded4a64c8fe8ef8bde2eb350e1e4b99b83e33
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-05-12 23:25:16 +0200

    feat: create `TripPreview` type

---
+++ src/app/shared/trip-preview.ts
@@ -0,0 +1,11 @@
+export type TripPreview = {
+  id: string;
+
+  author: string;
+  country: string;
+  ticketPrice: number;
+  hotelPrice: number;
+  totalPrice: number;
+  duration: string;
+  imageUrl: string;
+};
