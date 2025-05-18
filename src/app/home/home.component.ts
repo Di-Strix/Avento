07dc31f88b7dc6bc3dde2c983c82e03bede37355
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-05-19 00:00:43 +0200

    feat: implement home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,51 @@
 import { Component } from '@angular/core';
+import { MatFabButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+import { TripPreview } from '../shared/trip-preview';
+
+import { TripPreviewCardComponent } from './trip-preview-card/trip-preview-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TripPreviewCardComponent, MatFormFieldModule, MatInputModule, MatFabButton, MatCardModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  trips: TripPreview[] = [
+    {
+      id: '1',
+      author: 'minkx',
+      country: 'Kazakhstan',
+      duration: '2',
+      hotelPrice: 100,
+      totalPrice: 150,
+      ticketPrice: 50,
+      imageUrl: 'countries/Kazakhstan.jpg',
+    },
+    {
+      id: '2',
+      author: 'estonista',
+      country: 'Estonia',
+      duration: '2',
+      hotelPrice: 100,
+      totalPrice: 175,
+      ticketPrice: 50,
+      imageUrl: 'countries/Estonia.jpg',
+    },
+    {
+      id: '3',
+      author: 'John',
+      country: 'Brazil',
+      duration: '2',
+      hotelPrice: 100,
+      totalPrice: 300,
+      ticketPrice: 50,
+      imageUrl: 'countries/Brazil.jpg',
+    },
+  ];
+}
