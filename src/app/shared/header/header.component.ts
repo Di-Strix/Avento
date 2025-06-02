d5db95235169d48219c1126855a7d9656d4ba2c3
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-06-02 18:33:51 +0200

    feat: implement `header` component

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbar, MatIcon, MatIconButton],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
