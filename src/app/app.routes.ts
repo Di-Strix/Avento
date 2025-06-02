5d825f7532217cc6661b30ad1a6600bab81d69b9
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-06-02 18:39:16 +0200

    feat: add `TripView` component to the router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { TripViewComponent } from './trip-view/trip-view.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'trip/:id', component: TripViewComponent },
   { path: '**', redirectTo: '' },
 ];
