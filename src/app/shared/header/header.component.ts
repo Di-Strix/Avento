ef014124f12c099c77b6f7128844c94613e541d1
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-06-02 18:46:56 +0200

    feat(Header): redirect to main the page when "Avento" clicked

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar, MatIcon, MatIconButton],
+  imports: [MatToolbar, MatIcon, MatIconButton, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
