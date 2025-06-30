8a61e4bdd3cc5ea42851dc5d8490e5945afcc28e
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-06-30 21:15:46 +0200

    feat(auth): implement `authGuard`

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+import { AuthService } from '../shared/auth/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.currentUser()) return true;
+
+  router.navigate(['/login'], {
+    queryParams: {
+      redirectTo: route.url,
+    },
+  });
+
+  return false;
+};
