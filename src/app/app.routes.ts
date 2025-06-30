499c90a80fb01cfda3faee61ae9968c80744551b
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-06-30 21:22:07 +0200

    feat(register): implement register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { TripViewComponent } from './trip-view/trip-view.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'trip/:id', component: TripViewComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,135 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, RouterLink } from '@angular/router';
+
+import { merge, tap } from 'rxjs';
+
+import { AuthService } from '../shared/auth/auth.service';
+import { HeaderComponent } from '../shared/header/header.component';
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    HeaderComponent,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    RouterLink,
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+  @Input() redirectTo: string = '/';
+
+  loading: boolean = false;
+
+  credentials = new FormGroup({
+    email: new FormControl<string>('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+    username: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(3)],
+    }),
+    password: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(8)],
+    }),
+    repeatPassword: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(8), ValidatorSame('password')],
+    }),
+  });
+
+  constructor(
+    public authService: AuthService,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) {
+    // Update validity of all interlocked controls when any of them changes
+    const controls = this.credentials.controls;
+    const interlocked = [controls.password, controls.repeatPassword];
+    merge(...interlocked.map((control) => control.valueChanges)).subscribe(() => {
+      interlocked.forEach((control) => control.updateValueAndValidity({ emitEvent: false }));
+    });
+  }
+
+  usernameErrorMessage(): string {
+    const control = this.credentials.controls.username;
+
+    if (control.hasError('required')) return 'Username or email is required';
+    if (control.hasError('minlength')) return 'Username or email is too short';
+
+    return '';
+  }
+
+  passwordErrorMessage(control: AbstractControl<string>): string {
+    if (control.hasError('required')) return 'Password is required';
+    if (control.hasError('minlength')) return 'Password too short';
+    if (control.hasError('same')) return 'Passwords are not the same';
+
+    return '';
+  }
+
+  onSubmit() {
+    if (this.credentials.invalid) return;
+    if (this.loading) return;
+
+    const { username, password } = this.credentials.getRawValue();
+
+    this.credentials.disable();
+    this.loading = true;
+
+    this.authService
+      .register(username, password, this.redirectTo)
+      .pipe(
+        tap({
+          finalize: () => {
+            this.loading = false;
+            this.credentials.enable();
+          },
+        })
+      )
+      .subscribe({
+        error: (err: HttpErrorResponse) => {
+          this.snackBar.open(err.message, 'close', { duration: 3000 });
+          console.error(err);
+        },
+      });
+  }
+}
+
+function ValidatorSame(referenceControlPath: string): ValidatorFn {
+  return (control) => {
+    let parent = control;
+    if (!control.parent) return null;
+
+    while (parent.parent) {
+      parent = parent.parent;
+    }
+
+    const reference = parent.get(referenceControlPath);
+    if (!reference) throw new Error(`Reference control could to be found by path ${referenceControlPath}`);
+
+    if (reference.value === control.value) return null;
+    return { same: true };
+  };
+}
