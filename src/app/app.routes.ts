516d8072024dde7bc185806ab161a7419c3afef1
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-04 17:58:24 +0200

    feat(router): add `profile` path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
+import { authGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 import { TripViewComponent } from './trip-view/trip-view.component';
 
@@ -10,5 +12,6 @@ export const routes: Routes = [
   { path: 'trip/:id', component: TripViewComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '' },
 ];
