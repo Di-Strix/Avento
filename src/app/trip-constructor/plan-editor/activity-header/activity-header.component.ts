cc97275caf88b1c6efbd3510e29d19b9040655b0
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-07 16:00:12 +0200

    feat(activity-header): implement `ActivityHeaderComponent`

---
+++ src/app/trip-constructor/plan-editor/activity-header/activity-header.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { AirportNamePipe } from '../../../shared/airport/airport-name.pipe';
+import { AttractionNamePipe } from '../../../shared/attraction/attraction-name.pipe';
+import { StayNamePipe } from '../../../shared/stay/stay-name.pipe';
+import { TripForm } from '../../trip-form';
+
+@Component({
+  selector: 'app-activity-header',
+  imports: [CommonModule, MatIconModule, AirportNamePipe, StayNamePipe, AttractionNamePipe],
+  templateUrl: './activity-header.component.html',
+  styleUrl: './activity-header.component.scss',
+})
+export class ActivityHeaderComponent {
+  @Input({ required: true }) control!: TripForm.Plan.Item;
+
+  parseFromTo(control: TripForm.Plan.Item) {
+    const value = control.value;
+
+    if (value.type !== 'flight') return { from: '', to: '' };
+
+    const from = value.connections?.at(0)?.value || '';
+    const to = value.connections?.at(-1)?.value || '';
+
+    return { from, to };
+  }
+
+  parseStayId(control: TripForm.Plan.Item): string {
+    const value = control.value;
+    if (value.type !== 'stay') return '';
+
+    return value.hotel || '';
+  }
+
+  parseAttractionId(control: TripForm.Plan.Item): string {
+    const value = control.value;
+    if (value.type !== 'attraction') return '';
+
+    return value.attraction || '';
+  }
+}
