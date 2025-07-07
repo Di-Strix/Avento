22890f79987f38c8ab472caeb07e846653bfabd9
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-07 17:01:01 +0200

    feat(router): add trip constructor to the router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,18 @@ import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
+import { TripConstructorComponent } from './trip-constructor/trip-constructor.component';
 import { TripViewComponent } from './trip-view/trip-view.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'trip/:id', component: TripViewComponent },
+  {
+    path: 'trip',
+    children: [
+      { path: 'create', component: TripConstructorComponent, canActivate: [authGuard] },
+      { path: ':id', component: TripViewComponent },
+    ],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
