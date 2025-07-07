a70007964cdc5ef64d0a6df116d1a284544475a2
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-08 00:17:29 +0200

    feat(airport-service): update airport data

--- src/app/shared/airport/airports.ts
+++ src/app/shared/airport/airports.ts
@@ -1,19 +1,34 @@
 import { Trip } from '../trip';
 
 export const airports: Record<string, Trip.Plan.Flight.Entity> = {
-  '1': {
-    displayName: 'Vienna International Airport (VIE)',
-    cityId: '1',
-    id: '1',
+  '686c21624af0b6b6c2c554df': {
+    id: '686c21624af0b6b6c2c554df',
+    displayName: 'Vienna International Airport',
+    cityId: '686c21624af0b6b6c2c554d9',
   },
-  '2': {
-    displayName: 'Istanbul Airport (IST)',
-    cityId: '2',
-    id: '2',
+  '686c21624af0b6b6c2c554e0': {
+    id: '686c21624af0b6b6c2c554e0',
+    displayName: 'Almaty International Airport',
+    cityId: '686c21624af0b6b6c2c554da',
   },
-  '3': {
-    displayName: 'Almaty International Airport (ALA)',
-    cityId: '3',
-    id: '3',
+  '686c21624af0b6b6c2c554e1': {
+    id: '686c21624af0b6b6c2c554e1',
+    displayName: 'John F. Kennedy International Airport',
+    cityId: '686c21624af0b6b6c2c554db',
+  },
+  '686c21624af0b6b6c2c554e2': {
+    id: '686c21624af0b6b6c2c554e2',
+    displayName: 'Istanbul Airport',
+    cityId: '686c21624af0b6b6c2c554dc',
+  },
+  '686c21624af0b6b6c2c554e3': {
+    id: '686c21624af0b6b6c2c554e3',
+    displayName: 'Warsaw Chopin Airport',
+    cityId: '686c21624af0b6b6c2c554dd',
+  },
+  '686c21624af0b6b6c2c554e4': {
+    id: '686c21624af0b6b6c2c554e4',
+    displayName: 'Dubai International Airport',
+    cityId: '686c21624af0b6b6c2c554de',
   },
 };
