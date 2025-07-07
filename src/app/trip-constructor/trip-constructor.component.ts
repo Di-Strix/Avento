69e53457448b1b9450235a9c6ba17935d48871b0
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-08 01:45:02 +0200

    feat(trip-constructor): use `InfoEditorComponent`

--- src/app/trip-constructor/trip-constructor.component.ts
+++ src/app/trip-constructor/trip-constructor.component.ts
@@ -1,37 +1,25 @@
 import { Component } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MatFormField, MatInputModule } from '@angular/material/input';
 
 import { AuthService } from '../shared/auth/auth.service';
 import { HeaderComponent } from '../shared/header/header.component';
 
 import { createTripForm } from './helpers';
+import { InfoEditorComponent } from './info-editor/info-editor.component';
 import { PlanEditorComponent } from './plan-editor/plan-editor.component';
 import { TripForm } from './trip-form';
 
 @Component({
   selector: 'app-trip-constructor',
-  imports: [
-    HeaderComponent,
-    MatIconModule,
-    MatCardModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatFormField,
-    PlanEditorComponent,
-  ],
+  imports: [HeaderComponent, MatIconModule, MatCardModule, PlanEditorComponent, InfoEditorComponent],
   templateUrl: './trip-constructor.component.html',
   styleUrl: './trip-constructor.component.scss',
 })
 export class TripConstructorComponent {
   tripForm: TripForm;
 
-  constructor(
-    public authService: AuthService,
-    fb: NonNullableFormBuilder
-  ) {
+  constructor(public authService: AuthService) {
     this.tripForm = createTripForm();
   }
 }
