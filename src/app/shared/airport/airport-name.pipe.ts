917aae42aaf63b973c8228aefa205a3967fae43e
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-07 15:52:42 +0200

    fix(airport-name-pipe): improve input validation

--- src/app/shared/airport/airport-name.pipe.ts
+++ src/app/shared/airport/airport-name.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { Observable, map } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 
 import { AirportService } from './airport.service';
 
@@ -11,6 +11,8 @@ export class AirportNamePipe implements PipeTransform {
   constructor(private airportService: AirportService) {}
 
   transform(id: string): Observable<string | null> {
+    if (!id) return of(null);
+
     return this.airportService.get(id).pipe(map((response) => response && response.displayName));
   }
 }
