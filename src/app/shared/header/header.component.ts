029b3fb46590defe01338087e7d711c50769d288
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-08 23:23:33 +0200

    refactor(header): remove unused imports

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,5 @@
 import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
 import { Component, Input } from '@angular/core';
-import { MatIconButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 
@@ -9,7 +7,7 @@ import { UserMenuComponent } from '../user-menu/user-menu.component';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar, MatIcon, MatIconButton, RouterLink, UserMenuComponent],
+  imports: [MatToolbar, RouterLink, UserMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
