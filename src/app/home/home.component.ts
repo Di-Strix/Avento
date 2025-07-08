1acb709e6dd754974b5b2b13ac43921a6390677e
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-08 22:44:43 +0200

    feat(home): load trip cards from backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatFabButton, MatIconButton } from '@angular/material/button';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatFabButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,6 +9,7 @@ import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../shared/header/header.component';
 import { TripPreviewCardComponent } from '../shared/trip-preview-card/trip-preview-card.component';
 import { TripCard } from '../shared/trip/trip-cards';
+import { TripService } from '../shared/trip/trip.service';
 
 @Component({
   selector: 'app-home',
@@ -25,8 +26,14 @@ import { TripCard } from '../shared/trip/trip-cards';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   private readonly tripService = inject(TripService);
 
   trips: TripCard[] | null = null;
+
+  ngOnInit(): void {
+    this.tripService.loadTripsSuggestions().subscribe((cards) => {
+      this.trips = cards;
+    });
+  }
 }
