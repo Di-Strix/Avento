e29cce8b22f20270d7813a3127e8059b5b21a0b6
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-09 17:08:16 +0200

    feat(header): add notifications button

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,11 +3,12 @@ import { Component, Input } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 
+import { NotificationsPanelButtonComponent } from '../notifications-panel-button/notifications-panel-button.component';
 import { UserMenuComponent } from '../user-menu/user-menu.component';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar, RouterLink, UserMenuComponent],
+  imports: [MatToolbar, RouterLink, UserMenuComponent, NotificationsPanelButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
