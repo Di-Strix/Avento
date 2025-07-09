2124b68fc1417a9ddc260a6034d0f3a873edfc0e
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-09 08:21:34 +0200

    feat(router): register `EditProfileComponent`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { authGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EditProfileComponent } from './profile/edit-profile/edit-profile.component';
 import { ProfileComponent } from './profile/profile.component';
 import { PublicProfileComponent } from './public-profile/public-profile.component';
 import { RegisterComponent } from './register/register.component';
@@ -22,7 +23,21 @@ export const routes: Routes = [
   },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent, canMatch: [authGuard] },
+  {
+    path: 'profile',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ProfileComponent,
+      },
+      {
+        path: 'edit',
+        component: EditProfileComponent,
+      },
+    ],
+    canMatch: [authGuard],
+  },
   { path: 'user/:userId', component: PublicProfileComponent },
   { path: 'search', component: SearchComponent },
   { path: '**', component: PageNotFoundComponent },
