713235e28443d3f2b4e0de367cd6af3eb6a15be3
Di-Strix <52571870+Di-Strix@users.noreply.github.com>
2025-07-09 04:12:08 +0200

    feat(trip-preview-card): add like button

--- src/app/shared/trip-preview-card/trip-preview-card.component.ts
+++ src/app/shared/trip-preview-card/trip-preview-card.component.ts
@@ -9,26 +9,42 @@ import {
   MatCardTitleGroup,
 } from '@angular/material/card';
 
+import { TripLikeButtonComponent } from '../trip-like-button/trip-like-button.component';
 import { TripCard } from '../trip/trip-cards';
 
 @Component({
   selector: 'app-trip-preview-card',
-  imports: [MatCard, MatCardHeader, MatCardContent, MatCardTitleGroup, MatCardTitle, MatCardSubtitle],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitleGroup,
+    MatCardTitle,
+    MatCardSubtitle,
+    TripLikeButtonComponent,
+  ],
   templateUrl: './trip-preview-card.component.html',
   styleUrl: './trip-preview-card.component.scss',
 })
 export class TripPreviewCardComponent {
   @Input({ required: true }) trip!: TripCard;
   @Input() appearance: 'outlined' | 'raised' = 'raised';
+  @Input() showAuthor: boolean = true;
 
   @Input()
-  get vertical() {
-    return this._vertical;
-  }
   set vertical(value: BooleanInput) {
     this._vertical = coerceBooleanProperty(value);
   }
+  get vertical() {
+    return this._vertical;
+  }
   private _vertical: boolean = false;
 
-  @Input() showAuthor: boolean = true;
+  @Input() set disableLike(value: BooleanInput) {
+    this._disableLike = coerceBooleanProperty(value);
+  }
+  get disableLike() {
+    return this._disableLike;
+  }
+  private _disableLike: boolean = false;
 }
